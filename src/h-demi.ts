b2fd114f03ed9679e5c081ffaeb30f542c983cec
Logue <logue@hotmail.co.jp>
2023-06-06 18:52:39 +0900

    Include patch-package etc. in dependencies. Update dependencies. Specification change of h-demi.

--- src/h-demi.ts
+++ src/h-demi.ts
@@ -2,6 +2,7 @@
  * h-demi - h function for Vue 2 and 3
  *
  * @see {@link https://github.com/vueuse/vue-demi/issues/65}
+ * @experimental
  */
 
 import { h as hDemi, isVue2, type VNode, type VNodeData } from 'vue-demi';
@@ -19,23 +20,29 @@ const adaptOnsV3 = (
 
 /**
  * hDemi function.
+ *
+ * @param type - Tag name etc
+ * @param options - VNode data.
+ * @param children - Children dom or VNode component
  */
-export default function h(
+const h = (
   type: string | Record<any, any>,
   options: VNodeData,
   chidren?: any
-): VNode {
+): VNode => {
   if (isVue2) {
     return hDemi(type, options, chidren);
   }
-  const ons = options.on != null ? adaptOnsV3(options.on) : {};
+  const ons = options.on ? adaptOnsV3(options.on) : {};
 
   return hDemi(
     type,
     { ...options.props, ...options.domProps, ...ons },
     chidren
   );
-}
+};
 
 export const slot = (defaultSlots: any): any =>
   typeof defaultSlots === 'function' ? defaultSlots() : defaultSlots;
+
+export default h;
